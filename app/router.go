d3b3bb4f783e13a63b43daa3823a2db8b12ed7f4
asrofil <riidloa@gmail.com>
2022-02-15 22:13:58 +0700

    add router

---
+++ app/router.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	"orderapi/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter(h *handler.HandleFunc) *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Home",
+		})
+	})
+
+	v1 := r.Group("/api/v1")
+	{
+		order := v1.Group("/order")
+		{
+			order.GET("/:orderId", h.GetOrderById)
+			order.PUT("/:orderId", h.PutOrderById)
+			order.POST("/", h.PostOrder)
+			order.DELETE("/:orderId", h.DeleteOrderById)
+		}
+
+		orders := v1.Group("/orders")
+		{
+			orders.GET("/", h.GetMultipleOrder)
+			orders.PUT("/", h.PutMultipleOrder)
+			orders.DELETE("/", h.DeleteMultipleOrder)
+		}
+	}
+
+	return r
+}
