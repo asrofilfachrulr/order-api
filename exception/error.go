47a4a91413a60b689332af9b626048e682404be9
asrofil <riidloa@gmail.com>
2022-02-19 17:59:24 +0700

    in halfway scoping error and implementing validation

---
+++ exception/error.go
@@ -0,0 +1,32 @@
+package exception
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func BadRequestError(c *gin.Context, err error) {
+	c.AbortWithStatusJSON(400, gin.H{
+		"status":  "Bad Request",
+		"message": err.Error(),
+	})
+	panic(err)
+	// c.JSON(400, gin.H{
+	// 	"status":  "Bad Request",
+	// 	"message": err.Error(),
+	// })
+	// <-c.Request.Context().Done()
+}
+
+func NotFoundError(c *gin.Context, err error) {
+	c.JSON(404, gin.H{
+		"status":  "Not found",
+		"message": err.Error(),
+	})
+}
+
+func InternalServerError(c *gin.Context, err error) {
+	c.JSON(500, gin.H{
+		"status":  "Internal Error",
+		"message": err.Error(),
+	})
+}
