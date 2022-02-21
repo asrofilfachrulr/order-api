e0f21136d2154a9b3ecbe4c940799c52419e3a25
asrofil <riidloa@gmail.com>
2022-02-21 22:06:22 +0700

    update

--- main.go
+++ main.go
@@ -1,29 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"orderapi/app"
 	"orderapi/controller"
 	"orderapi/exception"
 	"orderapi/handler"
+	"orderapi/inmemory"
 	"orderapi/service"
 
 	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// db connectionP
 	db := app.NewDB()
 
-	m := service.NewMenuService(db)
+	// service
+	m := service.NewMenuService(db.DB)
 	err := m.LoadMenu()
 	exception.TerminateRuntimeIfError(err)
 
-	s := service.NewOrderService(db)
+	s := service.NewOrderService(db.DB)
+
+	// controller
 	c := controller.NewController(s)
+	// validator
 	v := validator.New()
+	// handler
 	h := handler.NewHandler(c, v)
+	// router
 	r := app.NewRouter(h)
 
-	// fmt.Println(inmemory.ListMenuInmemory)
+	fmt.Println(inmemory.ListMenuInmemory)
 
 	r.Run()
 }
