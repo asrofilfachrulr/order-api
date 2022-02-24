10a1c8a8789bcc99becd6ef87742c5d7ebd43622
asrofil <riidloa@gmail.com>
2022-02-24 08:34:03 +0700

    OrderUpdate Validation

--- model/orderupdate.go
+++ model/orderupdate.go
@@ -1,6 +1,45 @@
 package model
 
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+)
+
 type OrderUpdate struct {
-	Status string      `json:"status"`
-	Items  []OrderItem `json:"items"`
+	Status string      `json:"status" validate:"eq='paid'|'unpaid'|'conflict'"`
+	Items  []OrderItem `json:"items" validate:"dive"`
+}
+
+func (o *OrderUpdate) ParseJSON(i io.ReadCloser) (OrderUpdate, error) {
+	dataByte, err := ioutil.ReadAll(i)
+	if err != nil {
+		return OrderUpdate{}, err
+	}
+
+	newOrderUpdate := OrderUpdate{}
+
+	err = json.Unmarshal(dataByte, &newOrderUpdate)
+	if err != nil {
+		return OrderUpdate{}, err
+	}
+	return newOrderUpdate, nil
+}
+
+func (o *OrderUpdate) ValidateMissing() error {
+	missing := false
+
+	if v := o.Status; v == "" {
+		missing = true
+	}
+	if v := o.Items; len(v) == 0 {
+		missing = true
+	}
+
+	if missing {
+		return errors.New("attribute missing")
+	}
+
+	return nil
 }
